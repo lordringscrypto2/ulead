153ceb888428fe81911bf9ceb9c0661751579f56
hashwaveulead <hashwaveulead@gmail.com>
2018-12-11 22:25:24 +0000

    MN Collateral updated from 10K to 1.5M

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -449,7 +449,7 @@ vector<COutput> CActiveMasternode::SelectCoinsMasternode()
 
     // Filter
     BOOST_FOREACH (const COutput& out, vCoins) {
-        if (out.tx->vout[out.i].nValue == 10000 * COIN) { //exactly
+        if (out.tx->vout[out.i].nValue == 1500000 * COIN) { //exactly
             filteredCoins.push_back(out);
         }
     }
